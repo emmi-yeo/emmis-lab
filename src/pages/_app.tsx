f82853ee296759f64df0d0ac1ab0a1bf2556635e
Emmi <msminyi@gmail.com>
2025-10-28 11:06:56 +0800

    add web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import CustomCursor from '@/components/CustomCursor';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <CustomCursor />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
